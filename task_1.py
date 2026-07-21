e417aff17d26025bade02cc7c70641451b4acb6d
radheshamrathod788-hub <radheshamrathod788@gmail.com>
2026-07-21 22:36:38 +0530

    Add files via upload

---
+++ task_1.py
@@ -0,0 +1,7 @@
+a=int(input("enter the number"))
+if a>1:
+    print("Positive number")
+elif a==0:
+    print("zero")
+else:
+    print("Nagetive number")
---
+++ task_5.py
@@ -0,0 +1,16 @@
+num1=int(input("enter the num1:"))
+num2=int(input("enter the num2:"))
+num3=int(input("enter the num3:"))
+
+if num1>num2 and num1> num3:
+    print(num1, "is Greatest")
+elif num2>num1 and num2> num3:
+    print(num2, "is Greatest")
+else:
+    print(num3, "is Gretest")
+
+
+# 12
+# 56
+# 34
+# ==>56is grate
