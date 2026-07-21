aefeca6b822edf6f46d37309f2ee04a91fd69a6c
radheshamrathod788-hub <radheshamrathod788@gmail.com>
2026-07-21 22:38:11 +0530

    Add files via upload

---
+++ task_10.py
@@ -0,0 +1,13 @@
+marks=int(input("enter the marks:"))
+if marks>=90 and marks<=100:
+    print("A+")
+elif marks>=80 and marks<=89:
+    print("A")
+elif marks>=70 and marks<=79:
+    print("B")
+elif marks>=60 and marks<=69:
+    print("C")
+elif marks>=35 and marks<=59:
+    print("D")
+else:
+    print("Fail")
---
+++ task_9.py
@@ -0,0 +1,5 @@
+a=int(input("enter the number:"))
+if a>=35:
+    print("Pass")
+else:
+    print("fail")
