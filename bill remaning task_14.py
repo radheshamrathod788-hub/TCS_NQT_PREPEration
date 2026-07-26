da4a60b0b8e9b4ac4adba2e76eb738d8e0eb7056
Radhesham Rathod <radheshamrathod788@gmail.com>
2026-07-26 20:02:44 +0530

    Add files via upload

---
+++ bill remaning task_14.py
@@ -0,0 +1,10 @@
+units=int(input("enter the electricity bill units:"))
+
+if units <100:
+    print("Low bill")
+elif units>100 and units<300:
+    print("Medium Bill")
+
+elif
+else:
+    print("High Bill")
